a09d5fb72396afc1624d50ed37c88b29771835ce
ashley7414 <147580257+ashley7414@users.noreply.github.com>
2024-07-26 13:24:07 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Websites that allows developers to store and manage their code using git");
+            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
         }
     }
 }
