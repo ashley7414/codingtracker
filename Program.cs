d2b54ff328a992b21ec0e062b8c69efc603c1055
ashley7414 <147580257+ashley7414@users.noreply.github.com>
2024-07-26 13:29:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
+            
         }
     }
 }
