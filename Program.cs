a4a59e184615bd02c460ca841df7f92c80597c4d
ashley7414 <ashleshayadav453@gmail.com>
2024-07-26 13:38:30 +0530

    keeping oone message

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code");
+            Console.WriteLine("Websites that allows developers to store and manage their code using git and the git is a version control system tool that helps to track changes in a code insde to the program");
             
         }
     }
